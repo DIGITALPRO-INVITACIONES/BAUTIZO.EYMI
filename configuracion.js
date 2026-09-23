dc3e6b5ed4a18ec53eb58afd87ddd4e80b589cc5
DIGITALPRO-INVITACIONES <vigorex45@gmail.com>
2026-09-23 12:56:46 -0400

    Add files via upload

--- configuracion.js
+++ configuracion.js
@@ -1,1 +1,1 @@
-window.BAUTIZO={anio:2026,horaCeremonia:'',horaRecepcion:'',fechaCuentaRegresiva:'2026-10-10T00:00:00-04:00'};
+window.BAUTIZO={anio:2026,horaCeremonia:'14:00 · 2:00 p. m.',horaRecepcion:'15:30 · 3:30 p. m.',fechaCuentaRegresiva:'2026-10-10T14:00:00-04:00'};
